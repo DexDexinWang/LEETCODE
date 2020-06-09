f0697d370f5329be8f13d29396dee9998e1ec308
DexDexinWang <edgar_wdx@hotmail.com>
2020-06-09 12:49:05 -0700

    Update

--- LeetCodeTree/894. All Possible Full Binary Trees/Solution894.java
+++ LeetCodeTree/894. All Possible Full Binary Trees/Solution894.java
@@ -11,25 +11,35 @@ public class Solution894 {
 	}
 
     public List<TreeNode> allPossibleFBT(int N) {
-        List<TreeNode> res = new LinkedList<>();
-        if (N == 0 || N % 2 == 0) return res;
-        TreeNode root = new TreeNode(0);
-        generateTree(root, root, 1, N, res);
-        return res;
+        List<TreeNode>[] res = new LinkedList[N+1];
+        res[0] = new LinkedList<>();
+        res[1] = new LinkedList<>();
+        if(N == 0) return res[0];
+        res[0].add(null);
+        res[1].add(new TreeNode(0));
+        for(int i = 2; i <=N; i++) {
+            res[i] = new LinkedList<TreeNode>();
+            if(i % 2 == 1) {
+                for(int j = 1; j < i; j=j+2) {
+                    for(TreeNode left: res[j]) {
+                        for(TreeNode right: res[i - j - 1]) {
+                            TreeNode newNode = new TreeNode(0);
+                            newNode.left = left;
+                            newNode.right = clone(right);
+                            res[i].add(newNode);
+                        }
+                    }
+                }
+            }
+        }
+        return res[N];
     }
     
-    public void generateTree(TreeNode root, TreeNode cur, int count, int N, List<TreeNode> res) {
-    	if (count == N) {
-        	res.add(root);
-        }  
-        cur.left = new TreeNode(0);
-        cur.right = new TreeNode(0);
-        count += 2;
-        if (count < N) {
-            generateTree(root, cur.left, count, N, res);
-            generateTree(root, cur.right, count, N, res);
-        }
-        cur.left = null;
-        cur.right = null;
+    private TreeNode clone (TreeNode node) {
+        if(node == null) return null;
+        TreeNode n = new TreeNode(0);
+        n.left = clone(node.left);
+        n.right = clone(node.right);
+        return n;
     }
 }
