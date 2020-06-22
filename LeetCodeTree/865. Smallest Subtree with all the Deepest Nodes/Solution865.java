a55e469ab62f818e691199459ebe5a6fd1e16e03
DexDexinWang <edgar_wdx@hotmail.com>
2020-06-22 10:47:31 -0700

    Update

---
+++ LeetCodeTree/865. Smallest Subtree with all the Deepest Nodes/Solution865.java
@@ -0,0 +1,36 @@
+import generator.TreeNode;
+
+public class Solution865 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    //BFS Post Order
+    //1. DFS get the deepest value.
+    //2. DFS check each node. 
+    //left and right: expect the deepest nodes.
+    //current: if left res is not null and right res is not null return current. Otherwise return not null value. 
+    //Return: return the deepest node.
+    private int deepest = Integer.MIN_VALUE;
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) return root;
+        dfsUpdateDeepest(root, 0);
+        return dfsGetDeepestNode(root, 0);
+    }
+    
+    private void dfsUpdateDeepest(TreeNode root, int depth) {
+        if(root == null) return;
+        if(depth > deepest) deepest = depth;
+        dfsUpdateDeepest(root.left, depth + 1);
+        dfsUpdateDeepest(root.right, depth + 1);
+    }
+    
+    private TreeNode dfsGetDeepestNode(TreeNode root, int depth) {
+        if(root == null || depth == deepest) return root;
+        TreeNode left = dfsGetDeepestNode(root.left, depth + 1);
+        TreeNode right = dfsGetDeepestNode(root.right, depth + 1);
+        if(left != null && right != null) return root;
+        return left == null ? right : left;
+    }
+}
